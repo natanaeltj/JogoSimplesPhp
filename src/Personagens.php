c7918e4223bad8cc1b941204e984b9e94fd7a35d
Insonia <natanaelteixeiradejesus2@gmail.com>
2026-09-13 15:06:43 -0300

    merge

--- src/Personagens.php
+++ src/Personagens.php
@@ -7,7 +7,7 @@ abstract class Personagens // ataque, vida, armadura e poderes
 {
 
     public string $nameClass;
-    protected int $ataque;
+    protected int $ataque; //precisa de um metodo set e get
     protected float $vida;
     protected int $armadura;
     protected array $poderes;
