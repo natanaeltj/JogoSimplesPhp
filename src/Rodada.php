d6258bdb8bab1078705085b7e821f07fac42e078
Natanael Teixeira <natanaelteixeiradejesus2@gmail.com>
2026-09-23 16:18:38 -0300

    sla

--- src/Rodada.php
+++ src/Rodada.php
@@ -34,7 +34,7 @@ class Rodada // turno, monstro atual, acao do jogador e resultado do combate
         echo("Vida: {$result()[0]->getVida()} \n");
         echo("Armadura: {$result()[0]->getArmor()} \n");
         echo("------Status do Inimigo------\n");
-        for( $rodadaA = 0, $result[0]->getVida == 0 || $user->getHeroiEscolhido()->getVida == 0, $rodadaA++){
+        for($rodadaA = 0, $result[0]->getVida == 0 || $user->getHeroiEscolhido()->getVida == 0, $rodadaA++){
             echo "------| Rodada Atual: {$rodadaA}|------";
             echo "Lançar ataque - 1\n";
             echo "Lançar poder - 2\n"; // mostrar lista de poderes e quais estao disponiveis nessa rodada e quanto falta pra lancar um ataque
@@ -61,33 +61,10 @@ class Rodada // turno, monstro atual, acao do jogador e resultado do combate
                     echo("Caso queira outra opção digite 3:");
                     $opcao = readline("Digite aqui: ");
 
-                    switch ($opcao) {
-                        case 1:
-                            $result()[0]->setVida($user->getHeroiEscolhido()->poderes[0]->dano - $result[0]->getArmor());
-                        case 2:
-                            $result()[0]->setVida($user->getHeroiEscolhido()->poderes[1]->dano - $result[0]->getArmor());
-                            case 3:
-                                echo "Lançar ataque - 1\n";
-                                echo "Pular turno - 2\n";
-                                echo "Sair do jogo - 3\n";
-                                $opcao = readline("Digite aqui:");
-                                switch ($opcao) {
-                                    case 1:
-                                        return;
-                                    case 2:
-                                        break;
-                                        case 3:
-                                            exit;
-                                }
-
-                    }
-
-
                     echo(""); //$heroiEscolhido->poderes[0 ou 1]
                 case 3:
                     echo("Você pulou um turno");
                     break;
-
                 case 4:
                    $confirmacao = readline("Tem certeza que deseja sair?");
                    if ($confirmacao == "Sim") {
