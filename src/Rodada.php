cf359436190263d3e0ae96b288f4757b339c2e0e
Insonia <natanaelteixeiradejesus2@gmail.com>
2026-09-25 19:39:40 -0300

    fds

--- src/Rodada.php
+++ src/Rodada.php
@@ -34,7 +34,7 @@ class Rodada // turno, monstro atual, acao do jogador e resultado do combate
         echo("Vida: {$result()[0]->getVida()} \n");
         echo("Armadura: {$result()[0]->getArmor()} \n");
         echo("------Status do Inimigo------\n");
-        for($rodadaA = 0, $result[0]->getVida == 0 || $user->getHeroiEscolhido()->getVida == 0, $rodadaA++){
+        for($rodadaA = 0; $result[0]->getVida == 0 || $user->getHeroiEscolhido()->getVida == 0; $rodadaA++){
             echo "------| Rodada Atual: {$rodadaA}|------";
             echo "Lançar ataque - 1\n";
             echo "Lançar poder - 2\n"; // mostrar lista de poderes e quais estao disponiveis nessa rodada e quanto falta pra lancar um ataque
